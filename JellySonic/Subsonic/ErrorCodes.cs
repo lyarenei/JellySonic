a9fe9c21b6e4ccd07ea7d3c66829038da25e35a7
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2022-02-17 21:29:50 +0100

    Change types to string

--- JellySonic/Subsonic/ErrorCodes.cs
+++ JellySonic/Subsonic/ErrorCodes.cs
@@ -8,40 +8,40 @@ public static class ErrorCodes
     /// <summary>
     /// A generic error.
     /// </summary>
-    public const int Generic = 0;
+    public const string Generic = "0";
 
     /// <summary>
     /// Required parameter is missing.
     /// </summary>
-    public const int MissingParam = 10;
+    public const string MissingParam = "10";
 
     /// <summary>
     /// Incompatible Subsonic REST protocol version. Client must upgrade.
     /// </summary>
-    public const int IncompatibleClient = 20;
+    public const string IncompatibleClient = "20";
 
     /// <summary>
     /// Incompatible Subsonic REST protocol version. Server must upgrade.
     /// </summary>
-    public const int IncompatibleServer = 30;
+    public const string IncompatibleServer = "30";
 
     /// <summary>
     /// Wrong username or password.
     /// </summary>
-    public const int InvalidCredentials = 40;
+    public const string InvalidCredentials = "40";
 
     /// <summary>
     /// Token authentication not supported for LDAP users.
     /// </summary>
-    public const int TokenNotSupportedForLDAP = 41;
+    public const string TokenNotSupportedForLDAP = "41";
 
     /// <summary>
     /// User is not authorized for the given operation.
     /// </summary>
-    public const int NotAuthorized = 50;
+    public const string NotAuthorized = "50";
 
     /// <summary>
     /// The requested data was not found.
     /// </summary>
-    public const int DataNotFound = 70;
+    public const string DataNotFound = "70";
 }
