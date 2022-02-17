082bb34384fc464121c7269431a3f750d93f5b85
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2022-02-18 00:37:24 +0100

    Remove unused code

--- JellySonic/PluginController.cs
+++ JellySonic/PluginController.cs
@@ -2,7 +2,6 @@ using System.IO;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
-using JellySonic.Formatters;
 using JellySonic.Models;
 using JellySonic.Services;
 using MediaBrowser.Common.Extensions;
@@ -68,20 +67,6 @@ public class PluginController : ControllerBase
         {
             return false;
         }
-    }
-
-    /// <summary>
-    /// Custom ObjectResult converter - adds XML formatter.
-    /// </summary>
-    /// <param name="val">Object to serialize.</param>
-    /// <param name="status">HTTP status code.</param>
-    /// <returns>Result object to use as response.</returns>
-    private static ObjectResult ToObjectResult(object val, int status = StatusCodes.Status200OK)
-    {
-        var objectResult = new ObjectResult(val);
-        objectResult.Formatters.Add(new JellySonicXmlFormatter());
-        objectResult.StatusCode = status;
-        return objectResult;
     }
 
     // Taken from https://github.com/jellyfin/jellyfin-plugin-opds/blob/b78a8bcc979581fe92835235a2c0d59516b5df15/Jellyfin.Plugin.Opds/OpdsApi.cs#L294
