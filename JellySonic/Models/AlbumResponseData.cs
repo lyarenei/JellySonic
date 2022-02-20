c6305fbc72c7aa358875b3eb2a6eccb6677411dd
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2022-02-20 09:36:51 +0100

    Split song to song response data

--- JellySonic/Models/AlbumResponseData.cs
+++ JellySonic/Models/AlbumResponseData.cs
@@ -6,7 +6,6 @@ using System.Globalization;
 using System.Linq;
 using System.Xml.Serialization;
 using MediaBrowser.Controller.Entities.Audio;
-using static System.IO.Path;
 using static JellySonic.Utils.Utils;
 
 namespace JellySonic.Models;
@@ -122,28 +121,13 @@ public class AlbumResponseData : ResponseData
 /// foobar.
 /// </summary>
 [XmlRoot(ElementName="song")]
-public class Song
+public class Song : SongResponseData
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="Song"/> class.
     /// </summary>
     public Song()
     {
-        Id = string.Empty;
-        Parent = string.Empty;
-        Title = string.Empty;
-        AlbumName = string.Empty;
-        ArtistName = string.Empty;
-        IsDir = "false";
-        CoverArt = string.Empty;
-        Created = DateTime.Now;
-        Duration = "0";
-        BitRate = "0";
-        FileSize = "0";
-        FilenameSuffix = string.Empty;
-        ContentType = string.Empty;
-        IsVideo = "false";
-        Path = string.Empty;
         AlbumId = string.Empty;
         ArtistId = string.Empty;
         Type = "music";
@@ -156,23 +140,8 @@ public class Song
     /// Initializes a new instance of the <see cref="Song"/> class.
     /// </summary>
     /// <param name="audio">Audio item.</param>
-    public Song(Audio audio)
+    public Song(Audio audio) : base(audio)
     {
-        Id = audio.Id.ToString();
-        Parent = string.Empty;
-        Title = audio.Name;
-        AlbumName = audio.Album;
-        ArtistName = audio.Artists.FirstOrDefault(string.Empty);
-        IsDir = "false";
-        CoverArt = string.Empty;
-        Created = audio.DateCreated;
-        Duration = TicksToSeconds(audio.RunTimeTicks).ToString(NumberFormatInfo.InvariantInfo);
-        BitRate = (audio.TotalBitrate / 1000).ToString() ?? "0";
-        FileSize = audio.Size?.ToString(NumberFormatInfo.InvariantInfo) ?? "0";
-        FilenameSuffix = GetExtension(audio.Path)[1..];
-        ContentType = string.Empty;
-        IsVideo = "false";
-        Path = audio.Path;
         AlbumId = audio.AlbumEntity.Id.ToString();
         ArtistId = string.Empty;
         Type = "music";
@@ -180,99 +149,6 @@ public class Song
         Genre = audio.Genres.FirstOrDefault(string.Empty);
         TrackOrder = audio.IndexNumber?.ToString(NumberFormatInfo.InvariantInfo) ?? "0";
     }
-
-    /// <summary>
-    /// Gets or sets ID of the song.
-    /// </summary>
-    [XmlAttribute("id")]
-    public string Id { get; set; }
-
-    /// <summary>
-    /// Gets or sets something.
-    /// No idea what is this.
-    /// TODO - find out what is this.
-    /// </summary>
-    [XmlAttribute("parent")]
-    public string Parent { get; set; }
-
-    /// <summary>
-    /// Gets or sets name of the song.
-    /// </summary>
-    [XmlAttribute("title")]
-    public string Title { get; set; }
-
-    /// <summary>
-    /// Gets or sets album name of the song.
-    /// </summary>
-    [XmlAttribute("album")]
-    public string AlbumName { get; set; }
-
-    /// <summary>
-    /// Gets or sets artist name of the song.
-    /// </summary>
-    [XmlAttribute("artist")]
-    public string ArtistName { get; set; }
-
-    /// <summary>
-    /// Gets or sets if song is a directory.
-    /// </summary>
-    [XmlAttribute("isDir")]
-    public string IsDir { get; set; }
-
-    /// <summary>
-    /// Gets or sets cover art of the song.
-    /// </summary>
-    [XmlAttribute("coverArt")]
-    public string CoverArt { get; set; }
-
-    /// <summary>
-    /// Gets or sets date of song creation.
-    /// </summary>
-    [XmlAttribute("created")]
-    public DateTime Created { get; set; }
-
-    /// <summary>
-    /// Gets or sets duration of the song.
-    /// </summary>
-    [XmlAttribute("duration")]
-    public string Duration { get; set; }
-
-    /// <summary>
-    /// Gets or sets bitrate of the song.
-    /// </summary>
-    [XmlAttribute("bitRate")]
-    public string BitRate { get; set; }
-
-    /// <summary>
-    /// Gets or sets filesize of the song.
-    /// </summary>
-    [XmlAttribute("size")]
-    public string FileSize { get; set; }
-
-    /// <summary>
-    /// Gets or sets filename suffix of the song.
-    /// </summary>
-    [XmlAttribute("suffix")]
-    public string FilenameSuffix { get; set; }
-
-    /// <summary>
-    /// Gets or sets content type of the song.
-    /// TODO - probably audio/something.
-    /// </summary>
-    [XmlAttribute("contentType")]
-    public string ContentType { get; set; }
-
-    /// <summary>
-    /// Gets or sets if song is a video.
-    /// </summary>
-    [XmlAttribute("isVideo")]
-    public string IsVideo { get; set; }
-
-    /// <summary>
-    /// Gets or sets path to the song filename.
-    /// </summary>
-    [XmlAttribute("path")]
-    public string Path { get; set; }
 
     /// <summary>
     /// Gets or sets ID of the song's album.
---
+++ JellySonic/Models/SongResponseData.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Xml.Serialization;
+using MediaBrowser.Controller.Entities.Audio;
+using static System.IO.Path;
+using static JellySonic.Utils.Utils;
+
+namespace JellySonic.Models;
+
+/// <summary>
+/// A Subsonic song response data.
+/// </summary>
+public class SongResponseData : ResponseData
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SongResponseData"/> class.
+    /// </summary>
+    public SongResponseData()
+    {
+        Id = string.Empty;
+        Parent = string.Empty;
+        Title = string.Empty;
+        AlbumName = string.Empty;
+        ArtistName = string.Empty;
+        IsDir = "false";
+        CoverArt = string.Empty;
+        Created = DateTime.Now;
+        Duration = "0";
+        BitRate = "0";
+        FileSize = "0";
+        FilenameSuffix = string.Empty;
+        ContentType = string.Empty;
+        IsVideo = "false";
+        Path = string.Empty;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SongResponseData"/> class.
+    /// </summary>
+    /// <param name="audio">Audio item.</param>
+    public SongResponseData(Audio audio)
+    {
+        Id = audio.Id.ToString();
+        Parent = string.Empty;
+        Title = audio.Name;
+        AlbumName = audio.Album;
+        ArtistName = audio.Artists.FirstOrDefault(string.Empty);
+        IsDir = "false";
+        CoverArt = string.Empty;
+        Created = audio.DateCreated;
+        Duration = TicksToSeconds(audio.RunTimeTicks).ToString(NumberFormatInfo.InvariantInfo);
+        BitRate = (audio.TotalBitrate / 1000).ToString() ?? "0";
+        FileSize = audio.Size?.ToString(NumberFormatInfo.InvariantInfo) ?? "0";
+        FilenameSuffix = GetExtension(audio.Path)[1..];
+        ContentType = string.Empty;
+        IsVideo = "false";
+        Path = audio.Path;
+    }
+
+    /// <summary>
+    /// Gets or sets ID of the song.
+    /// </summary>
+    [XmlAttribute("id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets something.
+    /// No idea what is this.
+    /// TODO - find out what is this.
+    /// </summary>
+    [XmlAttribute("parent")]
+    public string Parent { get; set; }
+
+    /// <summary>
+    /// Gets or sets name of the song.
+    /// </summary>
+    [XmlAttribute("title")]
+    public string Title { get; set; }
+
+    /// <summary>
+    /// Gets or sets album name of the song.
+    /// </summary>
+    [XmlAttribute("album")]
+    public string AlbumName { get; set; }
+
+    /// <summary>
+    /// Gets or sets artist name of the song.
+    /// </summary>
+    [XmlAttribute("artist")]
+    public string ArtistName { get; set; }
+
+    /// <summary>
+    /// Gets or sets if song is a directory.
+    /// </summary>
+    [XmlAttribute("isDir")]
+    public string IsDir { get; set; }
+
+    /// <summary>
+    /// Gets or sets cover art of the song.
+    /// </summary>
+    [XmlAttribute("coverArt")]
+    public string CoverArt { get; set; }
+
+    /// <summary>
+    /// Gets or sets date of song creation.
+    /// </summary>
+    [XmlAttribute("created")]
+    public DateTime Created { get; set; }
+
+    /// <summary>
+    /// Gets or sets duration of the song.
+    /// </summary>
+    [XmlAttribute("duration")]
+    public string Duration { get; set; }
+
+    /// <summary>
+    /// Gets or sets bitrate of the song.
+    /// </summary>
+    [XmlAttribute("bitRate")]
+    public string BitRate { get; set; }
+
+    /// <summary>
+    /// Gets or sets filesize of the song.
+    /// </summary>
+    [XmlAttribute("size")]
+    public string FileSize { get; set; }
+
+    /// <summary>
+    /// Gets or sets filename suffix of the song.
+    /// </summary>
+    [XmlAttribute("suffix")]
+    public string FilenameSuffix { get; set; }
+
+    /// <summary>
+    /// Gets or sets content type of the song.
+    /// TODO - probably audio/something.
+    /// </summary>
+    [XmlAttribute("contentType")]
+    public string ContentType { get; set; }
+
+    /// <summary>
+    /// Gets or sets if song is a video.
+    /// </summary>
+    [XmlAttribute("isVideo")]
+    public string IsVideo { get; set; }
+
+    /// <summary>
+    /// Gets or sets path to the song filename.
+    /// </summary>
+    [XmlAttribute("path")]
+    public string Path { get; set; }
+}
