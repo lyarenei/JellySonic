970926e7690115f325a0e9f86f98896e7859ef6d
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2022-03-05 20:31:56 +0100

    Use collection in music folders model

--- JellySonic/Models/MusicFolders.cs
+++ JellySonic/Models/MusicFolders.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Xml.Serialization;
@@ -12,16 +11,14 @@ namespace JellySonic.Models;
 /// <summary>
 /// Subsonic MusicFolders data type.
 /// </summary>
-[SuppressMessage("Design", "CA1002:Do not expose generic lists", Justification = "XML serialization")]
-[SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "XML serialization")]
 public class MusicFolders : IResponseData
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="Models.MusicFolders"/> class.
+    /// Initializes a new instance of the <see cref="MusicFolders"/> class.
     /// </summary>
     public MusicFolders()
     {
-        Folders = new List<MusicFolder>();
+        Folders = new Collection<MusicFolder>();
     }
 
     /// <summary>
@@ -30,26 +27,13 @@ public class MusicFolders : IResponseData
     /// <param name="items">Collection of folder items.</param>
     public MusicFolders(IEnumerable<BaseItem> items)
     {
-        Folders = items.Select(item => new MusicFolder(item)).ToList();
+        Folders = new Collection<MusicFolder>(items.Select(item => new MusicFolder(item)).ToList());
     }
 
     /// <summary>
-    /// Gets or sets a collection of music folders.
-    /// </summary>
-    [XmlIgnore]
-    [JsonIgnore]
-    public IEnumerable<MusicFolder> Folders { get; set; }
-
-    /// <summary>
-    /// Gets or sets collection of music folders for serialization.
+    /// Gets a collection of music folders.
     /// </summary>
     [XmlElement("musicFolder")]
     [JsonPropertyName("musicFolder")]
-    [Browsable(false)]
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public List<MusicFolder> GenreListSerialize
-    {
-        get { return Folders.ToList(); }
-        set { Folders = value; }
-    }
+    public Collection<MusicFolder> Folders { get; }
 }
