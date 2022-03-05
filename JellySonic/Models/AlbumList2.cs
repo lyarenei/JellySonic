4f55ddf9f7963b93c9e4e25e82e6c3920acc59d3
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2022-03-05 23:23:01 +0100

    Refactor album list 2 model

--- JellySonic/Models/AlbumList2.cs
+++ JellySonic/Models/AlbumList2.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Xml.Serialization;
 using JellySonic.Types;
 using MediaBrowser.Controller.Entities;
@@ -10,8 +11,6 @@ namespace JellySonic.Models;
 /// <summary>
 /// Subsonic AlbumList2 data type.
 /// </summary>
-[SuppressMessage("Design", "CA1002:Do not expose generic lists", Justification = "XML serialization")]
-[SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "XML serialization")]
 public class AlbumList2 : IResponseData
 {
     /// <summary>
@@ -19,7 +18,7 @@ public class AlbumList2 : IResponseData
     /// </summary>
     public AlbumList2()
     {
-        Albums = new List<AlbumId3>();
+        Albums = new Collection<AlbumId3>();
     }
 
     /// <summary>
@@ -28,22 +27,13 @@ public class AlbumList2 : IResponseData
     /// <param name="items">Collection of items.</param>
     public AlbumList2(IEnumerable<BaseItem> items)
     {
-        Albums = items.Select(item => new AlbumId3(item));
+        Albums = new Collection<AlbumId3>(items.Select(item => new AlbumId3(item)).ToList());
     }
 
     /// <summary>
-    /// Gets or sets list of albums.
-    /// </summary>
-    [XmlIgnore]
-    public IEnumerable<AlbumId3> Albums { get; set; }
-
-    /// <summary>
-    /// Gets or sets list of albums for serialization.
+    /// Gets list of albums.
     /// </summary>
     [XmlElement("album")]
-    public List<AlbumId3> AlbumsSerialize
-    {
-        get { return Albums.ToList(); }
-        set { Albums = value; }
-    }
+    [JsonPropertyName("album")]
+    public Collection<AlbumId3> Albums { get; }
 }
